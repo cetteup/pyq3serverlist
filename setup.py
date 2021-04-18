3ea76ee39e73ba9f37e24a72543fa99743671f2d
cetteup <me@cetteup.com>
2021-04-18 13:35:41 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyq3serverlist',
-    version='0.1.1',
+    version='0.1.2',
     description='Simple Python library for querying Quake 3 based principal servers and their game servers',
     url='https://github.com/cetteup/pyq3serverlist',
     author='cetteup',
