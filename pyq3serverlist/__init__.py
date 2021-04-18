678bc33f61c3d8714c15b8c8ddde8ee548c99a35
cetteup <me@cetteup.com>
2021-04-18 10:49:32 +0200

    Added metadata to __init__.py

--- pyq3serverlist/__init__.py
+++ pyq3serverlist/__init__.py
@@ -2,4 +2,13 @@ from .connection import Connection
 from .masterserver import MasterServer
 from .server import Server
 
+"""
+pyq3serverlist.
+
+Simple Python library for querying Quake 3 based master servers and it's game servers.
+"""
+
+__version__ = '0.1.0'
+__author__ = 'cetteup'
+__credits__ = 'https://github.com/jacklul'
 __all__ = ['Connection', 'MasterServer', 'Server']
