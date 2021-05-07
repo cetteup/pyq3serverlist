7850304f8d006e5a02ca643b44fefa01e10a25ba
cetteup <me@cetteup.com>
2021-05-07 17:36:31 +0200

    Updated project metadata

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='pyq3serverlist',
     version='0.1.3',
     description='Simple Python library for querying Quake 3 based principal servers and their game servers',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/cetteup/pyq3serverlist',
+    project_urls={
+        'Bug Tracker': 'https://github.com/cetteup/pyq3serverlist/issues',
+    },
     author='cetteup',
     author_email='me@cetteup.com',
     license='MIT',
