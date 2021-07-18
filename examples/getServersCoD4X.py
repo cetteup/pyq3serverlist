a65074611a50ba06d7b492ff871e49b86d8a9518
cetteup <me@cetteup.com>
2021-07-18 17:19:52 +0200

    Updated CoD4x example

--- examples/getServersCoD4X.py
+++ examples/getServersCoD4X.py
@@ -3,7 +3,7 @@ import socket
 from pyq3serverlist import PrincipalServer
 from pyq3serverlist.exceptions import PyQ3SLError, PyQ3SLTimeoutError
 
-principal = PrincipalServer('cod4master.cod4x.me', 20810, 6, socket.SOCK_STREAM, b'\x00\x00\x00\x00\x04')
+principal = PrincipalServer('cod4master.cod4x.me', 20810, 6, network_protocol=socket.SOCK_STREAM, server_entry_prefix=b'\x00\x00\x00\x00\x04')
 
 servers = []
 try:
