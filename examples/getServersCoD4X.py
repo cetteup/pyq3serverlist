51afc54f7971aee119fbd4a8bcb36c3f5639ee0c
cetteup <me@cetteup.com>
2023-01-23 17:31:03 +0100

    docs: Update timeout option in CoD4X example

--- examples/getServersCoD4X.py
+++ examples/getServersCoD4X.py
@@ -2,12 +2,13 @@ import socket
 
 from pyq3serverlist import PrincipalServer, PyQ3SLError, PyQ3SLTimeoutError
 
-principal = PrincipalServer('cod4master.cod4x.ovh', 20810, network_protocol=socket.SOCK_STREAM)
+principal = PrincipalServer('cod4master.cod4x.ovh', 20810, network_protocol=socket.SOCK_STREAM, timeout=2.0)
 
 servers = []
 try:
-    servers = principal.get_servers(query_protocol=6, game_name='cod4x', keywords='full empty \x00',
-                                    server_entry_prefix=b'\x00\x00\x00\x00\x04', timeout=2)
+    servers = principal.get_servers(
+        query_protocol=6, game_name='cod4x', keywords='full empty \x00', server_entry_prefix=b'\x00\x00\x00\x00\x04'
+    )
     print(servers)
 except (PyQ3SLError, PyQ3SLTimeoutError) as e:
     print(e)
